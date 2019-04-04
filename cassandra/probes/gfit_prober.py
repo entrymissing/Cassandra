c6f5e240dec327d2eebf630d8e41f363f949a830
David Engel <entrymissing@gmail.com>
2019-04-04 20:10:25 +0200

    Update gfit prober
    Adding a long workout count and fixing linter

--- cassandra/probes/gfit_prober.py
+++ cassandra/probes/gfit_prober.py
@@ -1,39 +1,43 @@
-import datetime
 import time
 
+import datetime
+
 from cassandra.probes import base_prober
-from cassandra import google_api_lib as google_api
 from cassandra import google_api_credentials_lib as google_creds
 
 __PROBE_NAME = ['GFitProber']
 
 class GFitProber(base_prober.BaseProber):
-  def setup(self):
-    # Queries and suffixes
-    self.TIME_WINDOWS = self.parameters.get('time_windows', [24*7])
-    self.SUFFIXES = self.parameters.get('suffixes', ['7d'])
-    
-    # Assert that both are the same length
-    if not len(self.TIME_WINDOWS) == len(self.SUFFIXES):
-      raise KeyError('Queries %s and Suffixes %s are not the same length' %
-          (self.TIME_WINDOWS, self.SUFFIXES))
+    def setup(self):
+        # Queries and suffixes
+        self.TIME_WINDOWS = self.parameters.get('time_windows', [24*7])
+        self.SUFFIXES = self.parameters.get('suffixes', ['7d'])
 
-  def collect_data(self):
-    service = google_creds.connect_to_api('fitness', 'v1')
-    data = {}
-    now = time.time()
-    utcnow = datetime.datetime.utcnow()
-    
-    for hours, suffix in zip(self.TIME_WINDOWS, self.SUFFIXES):
-      ndays = utcnow - datetime.timedelta(hours = hours)
-      startTime = ndays.isoformat("T") + "Z"
-      response = service.users().sessions().list(userId='me', startTime=startTime).execute()
+        # Assert that both are the same length
+        if not len(self.TIME_WINDOWS) == len(self.SUFFIXES):
+            raise KeyError('Queries %s and Suffixes %s are not the same length' %
+                           (self.TIME_WINDOWS, self.SUFFIXES))
 
-      numWorkout = len(response['session'])
-      timeWorkout = 0
-      for s in response['session']:
-        timeWorkout += ((int(s['endTimeMillis'])-int(s['startTimeMillis'])) / 1000)
-      
-      data[self.prefix + '.num_workout.' + suffix] = [(now, numWorkout)]
-      data[self.prefix + '.time_workout.' + suffix] = [(now, timeWorkout)]
-    return data
+    def collect_data(self):
+        service = google_creds.connect_to_api('fitness', 'v1')
+        data = {}
+        now = time.time()
+        utcnow = datetime.datetime.utcnow()
+
+        for hours, suffix in zip(self.TIME_WINDOWS, self.SUFFIXES):
+            ndays = utcnow - datetime.timedelta(hours=hours)
+            start_time = ndays.isoformat("T") + "Z"
+            response = service.users().sessions().list(userId='me', startTime=start_time).execute()
+
+            num_workout = len(response['session'])
+            long_workout_count = 0
+            time_workout = 0
+            for session in response['session']:
+                time_workout += ((int(session['endTimeMillis'])-int(session['startTimeMillis'])) / 1000)
+                if time_workout >= 30:
+                    long_workout_count += 1
+
+            data[self.prefix + '.num_workout.' + suffix] = [(now, num_workout)]
+            data[self.prefix + '.num_long_workout.' + suffix] = [(now, long_workout_count)]
+            data[self.prefix + '.time_workout.' + suffix] = [(now, time_workout)]
+        return data
